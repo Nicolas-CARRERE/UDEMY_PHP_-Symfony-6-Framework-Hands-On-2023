bd818711e8b02f9c12803a4ed067f507dab284b0
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2023-03-19 14:27:47 +0100

    #72. Embedding Forms

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\UserProfile;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -54,7 +55,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+            ->add('userProfile', UserProfileType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
